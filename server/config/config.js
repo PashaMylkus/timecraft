db00f214d36723c0903676d217e8ea5bb24f462e
PavloMylkus1 <ppavlo82@gmail.com>
2024-09-11 18:01:36 +0300

    migration db

---
+++ server/config/config.js
@@ -0,0 +1,19 @@
+const { config } = require("dotenv");
+config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USER || "admin",
+    password: process.env.DB_PASSWORD || "root",
+    database: process.env.DB_NAME || "time_craft_db",
+    host: "localhost", // Замість 'db'
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: "db", // Використовується у Docker
+    dialect: "postgres",
+  },
+};
---
+++ server/migrations/20240911144936-create-user.js
@@ -0,0 +1,29 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      email: {
+        type: Sequelize.STRING,
+      },
+      password: {
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("now"), // для автоматичної дати
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Users");
+  },
+};
